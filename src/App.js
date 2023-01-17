5333821fb297164a05a32827ff72f95aa648a79d
Edavilanava <112099434+Edavilanava@users.noreply.github.com>
2023-01-17 09:36:44 -0500

    turn in

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+//Very easy
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: [
+        { name: 'Eduardo', number: '704-449-8571', dob: '07/11/2001' },
+        { name: 'Zuri', number: '704-444-5555', dob: '08/26/2008' },
+        { name: 'Luis', number: '704-555-6666', dob: '05/30/2001' }
+      ]
+    };
+  }
+
+  //Easy
+  render() {
+    const peopleComponents = this.state.people.map(person => (
+      <BasicInfo
+        name={person.name}
+        number={person.number}
+        dob={person.dob}
+      />
+    ));
+
+    //Medium 
+    return (
+      <div className="App">
+        {peopleComponents}
+      </div>
+    );
+  }
+}
+
+//Hard
+function BasicInfo(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Name: {props.name}</p>
+      <p>Number: {props.number}</p>
+      <p>Date of Birth: {props.dob}</p>
     </div>
   );
 }
